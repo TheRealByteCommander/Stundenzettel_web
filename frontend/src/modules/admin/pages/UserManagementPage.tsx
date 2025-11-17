4ddbb05d1307170aa2a4a0d4a5c5f0f971d46951
Matthias <mschmitz0815@googlemail.com>
2025-11-17 07:57:45 +0100

    Entferne ungenutzten useMemo Import aus UserManagementPage

--- frontend/src/modules/admin/pages/UserManagementPage.tsx
+++ frontend/src/modules/admin/pages/UserManagementPage.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react";
+import { useState } from "react";
 import { Alert } from "../../../components/ui/alert";
 import { Button } from "../../../components/ui/button";
 import {
